93c8bcdf56ab44ba00d439e62234d19ce698cdff
pw-02 <watters.patrick@outlook.com>
2024-04-24 21:18:19 +0000

    tidy up lambda creation

--- superdl/awslambda/batch_creation/app.py
+++ superdl/awslambda/batch_creation/app.py
@@ -1,84 +1,32 @@
-import torch
-import torchvision
 import base64
-import boto3
-import redis
+import json
 import zlib
 import concurrent.futures
-from PIL import Image
+from concurrent.futures import ThreadPoolExecutor
 from io import BytesIO
-import json
-import os
+
+import boto3
+import redis
+import torch
+import torchvision
+from PIL import Image
 
 # Externalize configuration parameters
 #REDIS_HOST = '172.17.0.2'
 #REDIS_HOST = 'host.docker.internal' #use this when testing locally on .dev container
 #REDIS_PORT = 6379
 
-REDIS_HOST =  "10.0.31.114"
-# REDIS_HOST =  "ec2-34-217-48-32.us-west-2.compute.amazonaws.com"
-
+# Configuration parameters
+REDIS_HOST = "10.0.29.229"
 REDIS_PORT = 6378
-
-s3_client = boto3.client('s3')
-redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # Instantiate Redis client
-
-use_local = False
-
-def download_file(bucket_name, file_path):
-# Print current working directory
-    if use_local:
-        os.chdir('/workspaces/super-dl/')
-        file_path = os.path.join(os.getcwd(), file_path)
-        with open(file_path, 'rb') as file:
-            content = file.read()
-        return content
-    else:
-        # Download file into memory
-        obj = s3_client.get_object(Bucket=bucket_name, Key=file_path)
-        content = obj['Body'].read()
-        return content
-
-def process_file(content, transformations):
-    image = Image.open(BytesIO(content))
-    
-    if image.mode == "L":
-        image = image.convert("RGB")
-    # Apply transformations if specified
-    if transformations is not None:
-        processed_tensor = transformations(image)
-    else:
-        # Convert image to PyTorch tensor without transformations
-        processed_tensor =  torchvision.transforms.ToTensor()(image)
-    return processed_tensor
-
-def download_and_process_file(bucket_name, sample, transformations):
-    sample_path = sample[0]
-    sample_label = sample[1]
-    content = download_file(bucket_name, sample_path)
-    processed_tensor = process_file(content, transformations)
-    return processed_tensor, sample_label
-
-def create_torch_batch(bucket_name, batch_metadata, transformations):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        futures = {executor.submit(download_and_process_file, bucket_name, sample, transformations): sample for sample in batch_metadata}
-        sample_list = []
-        label_list = []
-
-        for future in concurrent.futures.as_completed(futures):
-            file_path = futures[future]
-            try:
-                processed_tensor, label = future.result()
-                sample_list.append(processed_tensor)
-                label_list.append(label)
-
-            except Exception as e:
-                print(f"Error processing file {file_path}: {e}")
-    return torch.stack(sample_list), torch.tensor(label_list)
+S3_CLIENT = boto3.client('s3')
+REDIS_CLIENT = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT) # Instantiate Redis client
 
 def dict_to_torchvision_transform(transform_dict):
+    """
+    Converts a dictionary of transformations to a PyTorch transform object.
+    """
     transform_list = []
-
     for transform_name, params in transform_dict.items():
         if transform_name == 'Resize':
             transform_list.append(torchvision.transforms.Resize(params))
@@ -91,59 +39,96 @@ def dict_to_torchvision_transform(transform_dict):
 
     return torchvision.transforms.Compose(transform_list)
 
+def is_image_file(path: str):
+    return any(path.endswith(extension) for extension in ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP'])
+    
+def get_data_sample(bucket_name, data_sample,transformations):
+    if S3_CLIENT is None:
+        S3_CLIENT = boto3.client('s3')
+
+    sample_path, sample_label = data_sample
+    obj = S3_CLIENT.get_object(Bucket=bucket_name, Key=sample_path)
+
+    if is_image_file(sample_path):
+        content = obj['Body'].read()
+        content = Image.open(BytesIO(content))
+        if content.mode == "L":
+            content = content.convert("RGB") 
+    else:
+        content = obj['Body'].read().decode('utf-8')
+
+    if transformations:
+        return transformations(content), sample_label
+    else:
+        return torchvision.transforms.ToTensor()(content), sample_label
+
+def create_minibatch(bucket_name, samples, transformations = None):
+    sample_data, sample_labels = [], []
+    with ThreadPoolExecutor() as executor:
+        futures = {executor.submit(get_data_sample, bucket_name, sample, transformations): sample for sample in samples}
+        for future in concurrent.futures.as_completed(futures):
+            file_path = futures[future]
+            try:
+                processed_tensor, label = future.result()
+                sample_data.append(processed_tensor)
+                sample_labels.append(label)
+            except Exception as e:
+                print(f"Error processing file {file_path}: {e}")
+
+    minibatch = torch.stack(sample_data), torch.tensor(sample_labels)
+    
+    #Serialize the PyTorch tensor
+    buffer = BytesIO()
+    torch.save(minibatch, buffer)
+    # serialized_mini_batch = buffer.getvalue()
+    minibatch = zlib.compress(buffer.getvalue()) #use_compression:
+
+    # Encode the serialized tensor with base64
+    minibatch = base64.b64encode(minibatch).decode('utf-8')
 
 def lambda_handler(event, context):
+    """
+    AWS Lambda handler function that processes a batch of images from an S3 bucket and caches the results in Redis.
+    """
     try:
-        #Extract information from the event
-        # body = event['body']
-        # event = json.loads(body)
-
         bucket_name = event['bucket_name']
-        batch_metadata = event['batch_metadata']
+        batch_samples = event['batch_samples']
         batch_id = event['batch_id']
-        use_compression = True
+        task = event['warmup']
+        cache_address = event['cache_address']
+        cache_host, cache_port = cache_address.split(":")
 
-        if bucket_name == 'foo':
-            return {'statusCode': 200,'message': 'function_warmed'}
+        if bucket_name == 'task':
+            return {'statusCode': 200, 'message': 'function warmed'}
+        
+        elif task == 'vision_minibatch':
+            transformations = event.get('transformations')
+             #deserailize transfor,ations
+            if transformations:
+                transformations = dict_to_torchvision_transform(json.loads(transformations))
+            torch_minibatch = create_minibatch(bucket_name, batch_samples, transformations)
 
-        if 'transformations' in event:
-            transformations= dict_to_torchvision_transform(json.loads(event['transformations']))
-        else:
-            transformations = None
+        elif task == 'text_minibatch':
+            transformations = event.get('transformations')
+            if transformations:
+                transformations = dict_to_torchvision_transform(json.loads(transformations))
+            torch_minibatch = create_minibatch(bucket_name, batch_samples, transformations)
 
-        #'batch_tensor' contains a batch of PyTorch tensors representing the images
-        tensor_batch = create_torch_batch(bucket_name, batch_metadata, transformations)
+        if REDIS_CLIENT is None:
+            REDIS_CLIENT = redis.StrictRedis(host=cache_host, port=cache_port) # Instantiate Redis client
 
-        # Serialize the PyTorch tensor to binary data, then get the serialized data from the buffer
-        buffer = BytesIO()
-        torch.save(tensor_batch, buffer)
-        serialized_tensor_batch = buffer.getvalue()
-
-        if use_compression:
-            serialized_tensor_batch = zlib.compress(serialized_tensor_batch)
-
-        # Encode with base64
-        serialized_tensor_batch = base64.b64encode(serialized_tensor_batch).decode('utf-8')
-
-      # cache_batch in redis using batch_id
-        is_cached = False
-        try:
-            redis_client.set(batch_id, serialized_tensor_batch)
-            is_cached = True
-            message = f''        
-        except Exception as e:
-            is_cached = False
-            message = f"Failed to cache batch '{batch_id}' Error: {str(e)})"
-
+        # Cache minibatch in Redis using batch_id as the key
+        REDIS_CLIENT.set(batch_id, torch_minibatch)
+        
         return {'statusCode': 200,
                 'batch_id': batch_id,
-                'is_cached': is_cached,
-                'message': message}
-
+                'is_cached': True,
+                'message':  f" Sucessfully cached mininbatch '{batch_id}'"
+                }
     except Exception as e:
         return {
             'statusCode': 500,
             'batch_id': batch_id,
             'is_cached': False,
-            'message': f'Error: {str(e)}'
+            'message': f"Failed to create mininbatch '{batch_id}'. Error: {str(e)}"
         }
